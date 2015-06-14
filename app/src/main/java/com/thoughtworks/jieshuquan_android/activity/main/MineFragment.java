27516af441a0f5d56270e8cc011e275c12e8f9f8
Zhang Shuai <zhang.shuai@rea-group.com>
2015-06-14 15:44:32 +0800

    Add head view in mine fragment.

--- app/src/main/java/com/thoughtworks/jieshuquan_android/activity/main/MineFragment.java
+++ app/src/main/java/com/thoughtworks/jieshuquan_android/activity/main/MineFragment.java
@@ -7,34 +7,35 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.makeramen.roundedimageview.RoundedImageView;
 import com.thoughtworks.jieshuquan_android.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 public class MineFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+
+    private static final String ARG_USERNAME = "user_name";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
+    @InjectView(R.id.head_borrow)
+    TextView mBadgeBorrow;
+    @InjectView(R.id.head_return)
+    TextView mBadgeReturn;
+    @InjectView(R.id.head)
+    RoundedImageView mHeadView;
+
+    private String mUserName;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MineFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static MineFragment newInstance(String param1, String param2) {
         MineFragment fragment = new MineFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_USERNAME, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
@@ -48,7 +49,7 @@ public class MineFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            mUserName = getArguments().getString(ARG_USERNAME);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
@@ -56,8 +57,28 @@ public class MineFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mine, container, false);
+        View view = inflater.inflate(R.layout.fragment_mine, container, false);
+        ButterKnife.inject(this, view);
+        initHead();
+        return view;
+    }
+
+    private void initHead() {
+        int borrowCount = 1;
+        int returnCount = 29;
+
+        setBadgeView(borrowCount, mBadgeBorrow);
+        setBadgeView(returnCount, mBadgeReturn);
+        mHeadView.setImageResource(R.drawable.ic_tab_more);
+    }
+
+    private void setBadgeView(int borrowCount, TextView badgeView) {
+        if (borrowCount <= 0) {
+            badgeView.setVisibility(View.GONE);
+        } else {
+            badgeView.setText(String.valueOf(borrowCount));
+            badgeView.setVisibility(View.VISIBLE);
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
