c24b3b589c663e81633ee8c0a388cfe8b33ad505
wangdengwu <dengwu.wang@gmail.com>
2015-06-17 13:14:46 +0800

    add layout for borrow

--- app/src/main/java/com/thoughtworks/jieshuquan_android/activity/main/borrow/BorrowFragment.java
+++ app/src/main/java/com/thoughtworks/jieshuquan_android/activity/main/borrow/BorrowFragment.java
@@ -2,11 +2,17 @@ package com.thoughtworks.jieshuquan_android.activity.main.borrow;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.thoughtworks.jieshuquan_android.R;
 import com.thoughtworks.jieshuquan_android.activity.main.OnFragmentInteractionListener;
@@ -34,7 +40,20 @@ public class BorrowFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_borrow, container, false);
+
+        View inflate = inflater.inflate(R.layout.fragment_borrow, container, false);
+
+        GridView gridview = (GridView) inflate.findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this.getActivity()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(parent.getContext(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        return inflate;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -59,5 +78,60 @@ public class BorrowFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+}
+
+class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(dip2px(110), dip2px(160)));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.s1086045, R.drawable.s1495029,
+            R.drawable.s1106934, R.drawable.s1495029,
+            R.drawable.s1086045, R.drawable.s1086045,
+            R.drawable.s1106934, R.drawable.s1495029,
+            R.drawable.s1086045, R.drawable.s1106934,
+            R.drawable.s1106934, R.drawable.s1495029,
+            R.drawable.s1106934, R.drawable.s1495029,
+            R.drawable.s1106934
+    };
+
+    public int dip2px(float dpValue) {
+        final float scale = mContext.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 }
